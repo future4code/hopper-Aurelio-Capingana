03fa6f0c031f780a6972d1c85ee8d04b172a12cf
Aurelio Capingana <aurelio.capingana01@gmail.com>
2022-02-08 20:40:19 -0300

    novo desafio

---
+++ variaveis/index.js
@@ -0,0 +1,32 @@
+// let a = 10
+// let b = 10
+
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+
+// let horaTrabalhado = prompt("Quantas horas você trabalha por dia?")
+// let ganhoHora = prompt("Quanto você recebe por dia?")
+// alert(horaTrabalhado + ` horas de trabalho por dia, Voce recebe ${horaTrabalhado/ganhoHora} por hora`)
+
+// let nome = 
+// let idade = 
+// console.log(typeof nome);
+// console.log(typeof idade); ambos imprimem um undefined por não terem atribuições as variaveis
+
+
+let nome = prompt("Olá, qual é o seu nome?")
+let idade = prompt(nome + ' Por gentileza, qual a sua idade?')
+
+alert(nome + ' voce tem ' + idade + ' anos de idade')
+document.write(nome + ' voce tem ' + idade + ' anos de idade')
+console.log(nome + ' voce tem ' + idade + ' anos de idade') 
---
+++ variaveis/programa/programa.js
@@ -0,0 +1,9 @@
+prompt("hoje é segunda-feira?")
+prompt("falo apartir de que Brasil?")
+prompt("A cor da minha roupa é azul?")
+
+let dia = "Sexta-Feira"
+let país = "Brasil"
+let cor =  "Verde"
+
+document.write('Hoje é ' + dia +',  falo apartir do , ' + país + ' a cor da minha roupa é ' + cor + '; obrigado!!!' )
