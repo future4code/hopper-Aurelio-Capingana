d430c0d7cfb9a481a0b26951fced57d199e8292f
Aurelio Capingana <aurelio.capingana01@gmail.com>
2022-05-26 21:50:18 -0300

    exercicio feito

--- apis-labenusers/src/App.js
+++ apis-labenusers/src/App.js
@@ -1,13 +1,34 @@
 import React  from "react";
+import TelaCadastro from "./components/TelaCadastro";
+import TelaLista from "./components/TelaLista";
+
 
 class App extends React.Component{
 
-  render(){
+  state = {
+    telaAtual : "cadastro"
+  }
+
+  trocarDeTela = () =>{
+    this.setState({
+      telaAtual: this.state.telaAtual === "cadastro" 
+      ? "lista"
+      : "cadastro"
+    })
+  }
+
+  render() {
 
     return(
-      <>
-      
-      </>
+      <div>
+        
+         {
+           this.state.telaAtual === "cadastro" 
+           ? <TelaCadastro filhoUm={this.trocarDeTela} /> 
+           : <TelaLista filhoDois={this.trocarDeTela} />
+         }
+         
+      </div>
     )
   }
 }
