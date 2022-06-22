08e5ff0eb0f69354661ff3ae990d9a00b271c7de
Aurelio Capingana <aurelio.capingana01@gmail.com>
2022-06-22 05:55:43 -0300

    exercicio em andamento

--- modulo3/labex/labex/src/pages/HomePage.js
+++ modulo3/labex/labex/src/pages/HomePage.js
@@ -1,1 +1,19 @@
 /*Para o usuário escolher entre Área Administrativa e Lista de Viagens*/
+import React from "react";
+import '../Style/HomePage.css'
+
+const HomePage = (props) =>{
+    return(
+        <div className="header">
+            <p className="titulo">{props.titulo}</p>
+            <p className="titulo">{props.sub}</p>
+
+            <div className="btn">
+                <button>Ver Viagem</button>
+                <button>Area Administrativa</button>
+            </div>
+
+        </div>
+    )
+}
+export default HomePage
